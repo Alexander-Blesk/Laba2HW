7b568a6acb259f20e57a958b64e6c77bfd8176ff
Alexander-Blesk <alexander_vas09@mail.ru>
2026-09-21 20:00:37 +0300

    main2.c

--- main2.c
+++ main2.c
@@ -2,8 +2,12 @@
 #include <locale.h>
 main() {
 	setlocale(LC_CTYPE, "RUS");
-	int a = 10, b = 13, start = 75, last = 133, result;
+	int a, b, start = 75, last = 133, result;
+	printf("Ведите стоимость первых 75 газет:\n ");
+	scanf_s("%d", &a);
+	printf("Ведите стоимость газеты после 75 штук:\n ");
+	scanf_s("%d", &b);
 	result = start * a + ((last - start) * b);
-	printf("������� ���������� � 133 ����� %d ������", result);
+	printf("Мальчик заработает с 133 газет %d рублей", result);
 
 }
